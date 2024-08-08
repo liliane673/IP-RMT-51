2ca514bd2ad931960500b1d53085e1ab13594b0c
liliane673 <lilianeernestin673@gmail.com>
2024-08-08 08:47:39 +0700

    fix: debug deploy server

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,4 +1,5 @@
 const cron = require('node-cron');
+const axios = require('axios');
 
 let token = 'asdasd'
 
@@ -21,3 +22,34 @@ const inter = setInterval(() => {
 clearInterval(inter)
 
 console.log();
+
+async function getFSToken() {
+    const clientID = process.env.FAT_SECRET_CLIENT_ID
+    const clientSecret = process.env.FAT_SECRET_CLIENT_SECRET
+
+    const options = {
+        url: "https://oauth.fatsecret.com/connect/token",
+        method: "POST",
+        auth: {
+            username: clientID,
+            password: clientSecret,
+        },
+        headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            // authorization: `Username: ${process.env.fs_id} Password: ${process.env.fs_secret}`
+        },
+        data: {
+            grant_type: "client_credentials",
+            scope: "basic",
+        },
+        json: true,
+    }
+
+    let response = await axios(
+        options
+    );
+    return response;
+}
+
+getFSToken()
+console.log("run getToken ===>>>")
