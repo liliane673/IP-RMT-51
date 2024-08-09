3fdb50ea277c9c4b9f87ca975997a83a65d83cb1
liliane673 <lilianeernestin673@gmail.com>
2024-08-09 07:15:40 +0700

    fix : fix code

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:3000",
-    // baseURL: ""
+    // baseURL: "https://hack-healthy-recipes.liliane-ernestin.site"
 })
 
 export default axiosInstance
